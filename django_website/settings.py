68608003747b59b4958c0ffe1c8eaef7d6e91a4e
Padam Singh <121996785+MRPADAMSINGH@users.noreply.github.com>
2024-07-23 23:47:41 +0530

    Update settings.py

--- django_website/settings.py
+++ django_website/settings.py
@@ -142,8 +142,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
